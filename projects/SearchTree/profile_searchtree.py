fdc9ea07f98448a1a885d6f54dc019ddc6dd1be0
Carlos Limão <carlos.limao@gmail.com>
2022-12-14 19:29:50 +0000

    Improved version.

--- projects/SearchTree/profile_searchtree.py
+++ projects/SearchTree/profile_searchtree.py
@@ -1,5 +1,6 @@
 #
-# Profile search tree implementation.
+# Teste dos tempos de execução de vários algoritmos
+# sobre árvores de pesquisa.
 #
 # UC: AED
 # Author: CJL
@@ -12,44 +13,55 @@ import random
 atree = None
 
 
-def setup_tree(n):
-    """
-    Setup tree to use in tests.
-
-    :param n: Number of elements in tree.
-    :return:  None
-    """
+def setup_random_tree(n):
     global atree
 
+    atree = None
+
+    # Gerar uma lista aleatória de números entre 1 e n.
     rlist = random.sample(range(1, n+1), n)
-    for i in rlist:
+
+    # Inserir a lista na árvore.
+    for item in rlist:
         if atree is None:
-            atree = searchtree.SearchTree(i)
+            atree = searchtree.SearchTree(item)
         else:
-            atree.insert_i(i)
+            atree.insert_i(item)
+
+
+def setup_ordered_tree(n):
+    global atree
+
+    atree = None
+
+    # Gerar uma lista ordenada de números entre 1 e n.
+    olist = list(range(1, n+1))
+
+    # Inserir a lista na árvore.
+    for item in olist:
+        if atree is None:
+            atree = searchtree.SearchTree(item)
+        else:
+            atree.insert_i(item)
 
 
 def test_tree(n):
-    """
-    Test time to find value in tree.
-    The test executs a fixed number of operations in a tree
-    with n values.
-
-    :param n: Number of elements in tree (not used).
-    :return: None
-    """
     global atree
-    for i in range(100000):
-        atree.exists(i)
+
+    for i in range(1, 10000):
+        atree.exists_i(n)
 
 
-#
-# Test tree imlementation.
-# After inserting a random list of values in the tree, we expect search time to be O(lg n).
-#
 profile.profile_algorithm(test_tree,
                           [100000, 200000, 300000, 400000, 500000],
-                          "Árvore Binária - Pesquisa de valores.",
+                          "Árvores de Pesquisa Binária - Pesquisa em árvore com valores aleatórios.",
                           use_number_list=False,
-                          setup=setup_tree,
+                          setup=setup_random_tree,
+                          adjust_for_length=False)
+
+profile.profile_algorithm(test_tree,
+                          [10000, 20000, 30000, 40000, 50000],
+                          "Árvores de Pesquisa Binária - Pesquisa em árvore com valores ordenados.",
+                          use_number_list=False,
+                          setup=setup_ordered_tree,
                           adjust_for_length=False)
